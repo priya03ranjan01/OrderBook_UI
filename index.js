612f6746b5373b7dc1677249e98b9f562cbeb083
Priya Ranjan <83080755+priya03ranjan01@users.noreply.github.com>
2022-05-29 15:45:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -41,3 +41,16 @@ return true;
   $(".bs").on('click', function() {
     alert("Order successfully placed to " + $(this).val() + " " + quantity.value +" quantity of " + symbol.options[symbol.selectedIndex].text + " @ Rs. " + price.value + "/- \nOrderID: ")
     });
+
+    var user = document.querySelector(".enter_user").children[0];
+
+// var input = document.querySelector("#id");
+// input.addEventListener("keyup", (event) => {
+//   if (event.key === "Enter" & input.value != "") {
+// var h=document.createElement('h4')
+// h.innerHTML="User ID: " + input.value;
+//     user.replaceChild(h,input);
+//     h.style.backgroundColor="#ffff44"
+//     document.querySelector(".enter_user").style.backgroundColor="#ffff44";
+//   }
+// });
